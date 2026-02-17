9259364f82d92e15a01562ee5e323ef4b62d89af
François Delbrayelle <fdelbrayelle@kestra.io>
2026-02-17 09:48:42 +0100

    fix: meta.messager & meta.whatsapp categories

--- src/main/java/io/kestra/plugin/meta/whatsapp/package-info.java
+++ src/main/java/io/kestra/plugin/meta/whatsapp/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for WhatsApp notifications.",
-    categories = PluginSubGroup.PluginCategory.ALERTING
+    categories = PluginSubGroup.PluginCategory.BUSINESS
 )
 package io.kestra.plugin.meta.whatsapp;
 
