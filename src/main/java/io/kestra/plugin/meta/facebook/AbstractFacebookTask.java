72517fcf36730925c75d05f74d3e5a5213268c6c
Malay Dewangan <malaydewangan310@gmail.com>
2026-09-11 17:40:28 +0530

    chore(facebook): drop the now unused URL builder

--- src/main/java/io/kestra/plugin/meta/facebook/AbstractFacebookTask.java
+++ src/main/java/io/kestra/plugin/meta/facebook/AbstractFacebookTask.java
@@ -44,12 +44,6 @@ public abstract class AbstractFacebookTask extends Task implements RunnableTask<
     @PluginProperty(group = "connection")
     protected Property<String> apiBaseUrl = Property.ofValue("https://graph.facebook.com");
 
-    protected String buildApiUrl(RunContext runContext, String endpoint) throws Exception {
-        String rVersion = runContext.render(this.apiVersion).as(String.class).orElse("v24.0");
-        String rBaseUrl = runContext.render(this.apiBaseUrl).as(String.class).orElse("https://graph.facebook.com");
-        return String.format("%s/%s/%s", rBaseUrl, rVersion, endpoint);
-    }
-
     /** The seven argument constructor is the only seam for the base URL, which apiBaseUrl has always controlled. */
     protected APIContext apiContext(RunContext runContext) throws Exception {
         var rToken = runContext.render(this.accessToken).as(String.class).orElseThrow();
